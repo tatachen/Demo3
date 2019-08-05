71e841bcb297aaab1f1c42eb6e83673e5c30ff02
tatachen <tata9876@gmail.com>
2019-08-05 17:36:47 +0800

    slider - winnie & honey

--- Demo3/ViewController.swift
+++ Demo3/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var winnieImageView: UIImageView!
+    @IBOutlet weak var honeyImageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func sliderChange(_ sender: Any) {
+        winnieImageView.frame.origin.x = CGFloat(60 + 315*slider.value);
+        honeyImageView.frame.origin.x = CGFloat(727 - 315*slider.value);
+        if slider.value == 1 {
+            winnieImageView.image = UIImage(named: "winnie2");
+    
+            honeyImageView.alpha = 0;
+        } else {
+            winnieImageView.image = UIImage(named: "winnie")
+            honeyImageView.alpha = 1;
+        }
+        
+    }
 }
 
